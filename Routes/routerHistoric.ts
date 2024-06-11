b2079f02debf771e52eba8450e0b19a95554c7db
DaltonGuima <dalton.guimaraes500@gmail.com>
2024-06-11 10:58:50 -0300

    Select pertence

--- Routes/routerHistoric.ts
+++ Routes/routerHistoric.ts
@@ -67,7 +67,7 @@ historicRouter.get('/myHistorics', async (request, response) => {
         try {
 
             const historics = await Historic.find({ user: (token as UserInterface).id })
-                .populate({ path: "tipoSimulado", populate: { path: "materia assunto", select: "nome" } })
+                .populate({ path: "tipoSimulado", populate: { path: "materia assunto", select: "nome pertence" } })
             return response.status(200).json(historics)
 
         } catch (error) {
