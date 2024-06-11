fff191f25596c6d591c095e35ba0b4ccf997ebe3
DaltonGuima <dalton.guimaraes500@gmail.com>
2024-06-11 12:51:14 -0300

    Popular questoes

--- Routes/routerHistoric.ts
+++ Routes/routerHistoric.ts
@@ -67,7 +67,8 @@ historicRouter.get('/myHistorics', async (request, response) => {
         try {
 
             const historics = await Historic.find({ user: (token as UserInterface).id })
-                .populate({ path: "tipoSimulado", populate: { path: "materia assunto", select: "nome pertence" } })
+                .populate({ path: "tipoSimulado ", populate: { path: "materia assunto", select: "nome pertence" } })
+                .populate({ path: "questoesFeitas ", populate: { path: "questao" } })
             return response.status(200).json(historics)
 
         } catch (error) {
